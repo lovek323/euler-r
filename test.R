4094e2e8f09b662053330e46d423d6a8e67126a6
lovek323 <lovek323@gmail.com>
2012-10-22 17:55:59 +1100

    Handle bigz
    In order to handle big integers (package: <code>gmp</code>) in our
    tests, we need to use checkTrue instead of checkEquals (which only
    allows the numeric data type).

--- test.R
+++ test.R
@@ -4,7 +4,7 @@ test.wrapper <- function(correctValue, problemNumber) {
 	startTime <- as.numeric(Sys.time())
 	# check that we have solved the problem correctly
 	actualValue <- do.call(paste('problem', problemNumber, sep = ''), list())
-	checkEquals(correctValue, actualValue)
+	checkTrue(correctValue == actualValue)
 	endTime <- as.numeric(Sys.time())
 	# check that we have taken less than 60 seconds to solve the problem
 	checkTrue(endTime - startTime < 60)
