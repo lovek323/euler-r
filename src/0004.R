7282357c099f72699f43054833fc51fd16fe3811
lovek323 <lovek323@gmail.com>
2012-10-22 18:25:46 +1100

    Added problem 4

---
+++ src/0004.R
@@ -0,0 +1,32 @@
+problem4 <- function() {
+	maxPalindrome = 0
+	for (i in 100:999) {
+		# we have already tried all multiplications of i and integers in 1:i
+		for (j in i:999) {
+			foo = i * j
+			# no need to test whether we have a palindrome if it's smaller than
+			# our current max
+			if (foo <= maxPalindrome) next
+			if (isPalindrome(foo)) maxPalindrome = foo
+		}
+	}
+	return(maxPalindrome)
+}
+
+isPalindrome <- function(foo) {
+	foo <- as.character(foo)
+	foo <- strsplit(foo, "")
+	foo <- foo[[1]]
+	oof <- rev(foo)
+	bar <- length(foo)
+
+	if (bar %% 2 == 0) bar <- bar / 2
+	else bar <- (bar + 1) / 2
+
+	for (i in 1:bar) if (foo[i] != oof[i]) return(FALSE)
+
+	return(TRUE)
+}
+
+problem4()
+
---
+++ tests/0004.R
@@ -0,0 +1,6 @@
+source('src/0004.R')
+
+test.problem4 <- function() {
+	test.wrapper(906609, '4')
+}
+
